e4bc96e643ca972a6ded76f096ee1c7c124a8d7b
Joshua Rodriguez <ewized@gmail.com>
2014-02-08 09:56:29 -0800

    Added a config file and let the messages be random

---
+++ src/main/java/net/year4000/serverlist/Configuration.java
@@ -0,0 +1,51 @@
+package net.year4000.serverlist;
+
+import net.cubespace.Yamler.Config.Config;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Configuration extends Config {
+    public Configuration(ServerList plugin) {
+        CONFIG_HEADER = new String[]{"ServerList Configuration"};
+        CONFIG_FILE = new File(plugin.getDataFolder(), "config.yml");
+        try {
+            init();
+        } catch (Exception e) {
+            plugin.log("The config has an error in it could not init it.");
+        }
+    }
+
+    private String prefix = "&3[&bY4K&3]&r ";
+    private String nonplayer = "&7Join us today!";
+    private String player = "&7Welcome back player!";
+
+    private List<String> messages;
+    {
+        messages = new ArrayList<>();
+        messages.add("&6Join our Survival Server!");
+        messages.add("&6Join our Creative Server!");
+        messages.add("&6Join our Games Server!");
+    }
+
+    // Get the prefix for the fist line
+    public String getPrefix() {
+        return prefix;
+    }
+
+    // Get the bottom line when no player is found.
+    public String getNonplayer() {
+        return nonplayer;
+    }
+
+    // Get the message when the player is found.
+    public String getPlayer() {
+        return player;
+    }
+
+    // Get the messages to show on the fist line.
+    public List<String> getMessages() {
+        return messages;
+    }
+}
--- src/main/java/net/year4000/serverlist/ServerList.java
+++ src/main/java/net/year4000/serverlist/ServerList.java
@@ -1,76 +1,80 @@
 package net.year4000.serverlist;
 
-import java.util.HashMap;
-
-import net.md_5.bungee.api.*;
-import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.connection.PendingConnection;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
+
 public class ServerList extends Plugin implements Listener {
-    HashMap<String, String> IPS = new HashMap();
-    final String MOTD_ONE = "&3[&bY4K&3] &a&oSurvival &2&o| &a&oCreative &2&o| &a&oPVP &2&o| &a&oGames";
-    final String MOTD_TWO = "&7Join the community! facebook.com/year4000";
-    final String MOTD_PLAYER = "&7Welcome back player!";
+    HashMap<String, String> IPS = new HashMap<String, String>();
+    Configuration config;
 
     @Override
     public void onEnable() {
-        this.getProxy().getPluginManager().registerListener( this, this );
+        config = new Configuration(this);
+        getProxy().getPluginManager().registerListener(this, this);
+        log("Enabled");
     }
 
     @EventHandler
-    public void onPostLogin( PostLoginEvent event ) {
+    public void onPostLogin(PostLoginEvent event) {
         ProxiedPlayer player = event.getPlayer();
-        String name = player.getName();
-        String ip = player.getAddress().toString();
-        this.IPS.put( ip, name );
+        IPS.put(player.getAddress().toString(), player.getName());
     }
 
     @EventHandler
-    public void onServerPing( ProxyPingEvent event ) {
-        // Event classes
+    public void onServerPing(ProxyPingEvent event) {
+        // Event variables
         ServerPing response = event.getResponse();
         PendingConnection connection = event.getConnection();
-
-        // Event vars
         String ip = connection.getAddress().toString();
-        String motd = replaceColor( MOTD_ONE );
-        if ( getPlayer ( ip ) != null ) {
-            String motdPlayer = replaceColor( MOTD_PLAYER );
-            motdPlayer = motdPlayer.replaceAll( "player",  getPlayer( ip ) );
-            motd = motd + " \n" + motdPlayer;
-        }
-        else {
-            motd = motd + " \n" + replaceColor( MOTD_TWO );
+        String motd = replaceColor(config.getPrefix());
+        Random rand = new Random(System.currentTimeMillis());
+
+        // Load the random messages top layer.
+        List<String> messages = config.getMessages();
+        String message = messages.get(Math.abs(rand.nextInt() % messages.size()));
+        motd += replaceColor(message);
+
+        // Load the player bottom layer.
+        if (getPlayer(ip) != null) {
+            String motdPlayer = replaceColor(config.getPlayer());
+            motd += " \n" + motdPlayer.replaceAll("player", getPlayer(ip));
+        } else {
+            motd += " \n" + replaceColor(config.getNonplayer());
         }
 
         // Set the MOTD
-        response.setDescription( motd );
-        event.setResponse( response );
-    }
-
-    // Make the IP Readable
-    public String readIp( String ip ) {
-        return ip.substring(1).split(":")[0];
+        response.setDescription(motd);
+        event.setResponse(response);
     }
 
     // Get the player's username with the given IP
-    public String getPlayer( String ip ) {
-        if ( this.IPS.containsKey( ip ) ) {
-            return this.IPS.get( ip );
-        }
-        else {
+    public String getPlayer(String ip) {
+        if (IPS.containsKey(ip)) {
+            return IPS.get(ip);
+        } else {
             return null;
         }
     }
 
     // Replace any color defined by Minecraft.
-    public String replaceColor( String msg ) {
+    public String replaceColor(String msg) {
         final char COLOR_CHAR = '\u00A7';
         return msg.replaceAll("&([0-9a-fA-Fk-rK-R])", COLOR_CHAR + "$1");
+    }
+
+    // Log an info message
+    public void log(String message) {
+        getLogger().log(Level.INFO, message);
     }
 }
